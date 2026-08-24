3cc8dfa56897517adb212cb747a7acbf4620bd3b
Sathvik <sathvik.hosatti@gmail.com>
2026-08-24 20:37:17 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// ==========================================
+// RAJATHADRI PALACE
+// SUPABASE CONFIGURATION
+// ==========================================
+
+const SUPABASE_URL =
+    "PASTE_YOUR_PROJECT_URL_HERE";
+
+const SUPABASE_ANON_KEY =
+    "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+
+
+// ==========================================
+// HOTEL UPI DETAILS
+// ==========================================
+
+const HOTEL_UPI_ID =
+    "PASTE_HOTEL_UPI_ID_HERE";
+
+const HOTEL_UPI_NAME =
+    "RAJATHADRI PALACE";
