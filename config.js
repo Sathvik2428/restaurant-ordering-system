284a7933da4cfec6be89a1ec520a046a602920f2
Sathvik <sathvik.hosatti@gmail.com>
2026-08-25 20:42:36 +0530

    Update Supabase URL in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 // ==========================================
 
 const SUPABASE_URL =
-    "https://rjuwxrxlskpyfbclurwy.supabase.co/rest/v1/";
+    "https://rjuwxrxlskpyfbclurwy.supabase.co";
 
 const SUPABASE_ANON_KEY =
     "sb_publishable_btHknkMsgvSk1hBgADI1yA_sZW9YL2Y";
